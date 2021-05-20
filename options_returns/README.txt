2bff812e6971f1e576ca3ce4ef216c08604b51db
Richard Todd <rwtodd@users.noreply.github.com>
2021-05-20 16:36:17 -0500

    added options premium calculation

---
+++ options_returns/README.txt
@@ -0,0 +1,7 @@
+		  ~~#:  O P T I O N S  R E T U R N S  :#~~
+
+I'm making python utilities to help me gauge annualized options premium
+returns.  On the windows side, I use powershell scripts, but I thought python
+would be better in the linux world.  And anyway, it was easy and fun.
+
+-- vim: set tw=77 : --
---
+++ options_returns/new-shortput.py
@@ -0,0 +1,50 @@
+#!/bin/env python
+
+from datetime import date, datetime, timedelta
+import math
+
+# some constants
+TODAY        = date.today()
+DAYS_IN_YEAR = 261
+
+def weekdays_between(d1,d2):
+    "Weekdays between two dates, inclusive"
+    day_count = 1 + (d2 - d1).days
+    wday = d1.weekday()
+    ceil = math.ceil
+    # wday 5 is Saturday, 6 is Sunday, so +1/+2 to get to 7
+    return day_count + ceil((wday+1)/7) - ceil((wday+1+day_count)/7) + \
+                       ceil((wday+2)/7) - ceil((wday+2+day_count)/7)
+
+def annualized(strike, prem, days):
+    return 100 * math.pow(1.0 + (prem / strike), DAYS_IN_YEAR / days) - 100
+
+def parse_date(d):
+    """Parse a date loosely, which could be given as '' == TODAY, or
+    dd == day dd of the current month, or
+    mm-dd == day dd of month mm of the current year, or
+    yyyy-mm-dd == the given date."""
+    if d == '': return TODAY
+    provided =  [int(part) for part in d.split('-')]
+    if len(provided) == 2:
+        provided = [TODAY.year] + provided
+    elif len(provided) == 1:
+        provided = [TODAY.year, TODAY.month] + provided
+    elif len(provided) > 3:
+        raise ValueError('Bad Date format! ' + d)
+    return date(*provided)
+
+if __name__ == '__main__':
+    FRIDAY = TODAY + timedelta(((4 - TODAY.weekday()) + 7) % 7)
+    import argparse
+    parser = argparse.ArgumentParser(description='Determine the annualized value of an option premium.')
+    parser.add_argument('strike', type=float, help='the strike price')
+    parser.add_argument('premium', type=float, help='the premium collected')
+    parser.add_argument('-e','--expires', type=parse_date, default=FRIDAY, help='the expiration date (defaults to this friday)')
+    parser.add_argument('-o','--open', type=parse_date, default=TODAY, help='the date the position is opened (defaults to today)')
+    args = parser.parse_args()
+    days = weekdays_between(args.open, args.expires)
+    strike, prem = args.strike, args.premium
+    result = annualized(strike, prem, days)
+    print(f'{result:.2f}%  # Strike price: ${strike}  Premium: ${prem}  Days in market: {days}')
+
