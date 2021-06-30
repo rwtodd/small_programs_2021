b3c92d21667c6c7f782bb1f5799315c0a4c6d7d0
Richard Todd <rwtodd@users.noreply.github.com>
2021-06-30 16:59:03 -0500

    added subcommands to the swift option-returns utility

---
+++ swift_options_returns/README.md
@@ -0,0 +1,8 @@
+# option-returns
+
+A program that covers the same kind of roi-estimation utilities I already
+had implemented as a powershell module.  Since I don't use pwsh on the mac, I
+thought I would use the opportunity to learn some Swift programming.
+
+For this, I used Apple's command-line parsing library, which was a joy to use.
+
--- swift_options_returns/Sources/option-returns/main.swift
+++ swift_options_returns/Sources/option-returns/main.swift
@@ -27,9 +27,6 @@ func friday(after d: Date) -> Date {
                             direction: .forward)!
 }
 
-// ----------------------------------------------------------------------
-// M A I N  P R O G R A M
-// ----------------------------------------------------------------------
 func parseDate(_ string: String) throws -> Date {
     let formatter = DateFormatter()
     formatter.defaultDate = TODAY
@@ -47,7 +44,13 @@ func parseDate(_ string: String) throws -> Date {
     throw ValidationError("Format as yyyy-MM-dd or MM-dd!")
 }
 
-struct OptionReturns: ParsableCommand {
+// ----------------------------------------------------------------------
+// M A I N  P R O G R A M S:  S H O R T - P U T
+// ----------------------------------------------------------------------
+struct ShortPut: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Estimates annualized returns for short put option trades.")
+
     @Option(name: .shortAndLong, help: "the strike price.")
     var strike: Double
 
@@ -67,6 +70,78 @@ struct OptionReturns: ParsableCommand {
             annualized = 100.0 * pow(1.0 + (premium / strike), PER_YEAR / Double(timeInMarket)) - 100.0
         print("\(String(format: "%.2f",annualized))% # Strike \(strike), Premium \(premium), \(timeInMarket) days in market")
     }
+}
+
+// ----------------------------------------------------------------------
+// M A I N  P R O G R A M S:  C O V E R E D - C A L L
+// ----------------------------------------------------------------------
+struct CoveredCall: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Estimates annualized returns for covered call trades.")
+
+    @Option(name: .shortAndLong, help: "the strike price.")
+    var strike: Double
+
+    @Option(name: .shortAndLong, help: "the cost basis, per share.")
+    var basis: Double?
+
+    @Option(name: .shortAndLong, help: "the premium collected")
+    var premium: Double
+
+    @Option(name: .shortAndLong, help: "the expiration date", transform: parseDate)
+    var expires: Date?
+
+    @Option(name: .shortAndLong, help: "the date the position was opened", transform: parseDate)
+    var openDate: Date?
+
+    mutating func run() throws {
+        let date1 = openDate ?? calendar.startOfDay(for:Date()),
+            date2 = expires ?? friday(after:date1),
+            basis = self.basis ?? strike,
+            timeInMarket = weekdaysBetween(date1,date2),
+            hiAnnualized = 100.0 * pow(1.0 + ((strike - basis + premium) / basis), 
+                                       PER_YEAR / Double(timeInMarket)) - 100.0,
+            loAnnualized = 100.0 * pow(1.0 + (premium / basis),
+                                       PER_YEAR / Double(timeInMarket)) - 100.0
+        print("\(String(format: "%.2f",loAnnualized))% # Worthless Strike \(strike), Premium \(premium), \(timeInMarket) days in market")
+        print("\(String(format: "%.2f",hiAnnualized))% # Exercised for \(strike - basis) pnl")
+    }
+}
+
+// ----------------------------------------------------------------------
+// M A I N  P R O G R A M S:  T A R G E T - R O I
+// ----------------------------------------------------------------------
+struct TargetROI: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Estimates the premium needed to hit an annualized ROI")
+
+// [-StrikePrice] <Decimal> [[-ROI] <Decimal>] [-Expiration <DateTime>] [-SoldDate
+//     <DateTime>]
+    @Option(name: .shortAndLong, help: "the strike price.")
+    var strike: Double
+
+    @Option(name: .shortAndLong, help: "the desired annualized return % (defualt: 20)")
+    var roi: Double = 20
+
+    @Option(name: .shortAndLong, help: "the expiration date", transform: parseDate)
+    var expires: Date?
+
+    @Option(name: .shortAndLong, help: "the date the position was opened", transform: parseDate)
+    var openDate: Date?
+
+    mutating func run() throws {
+        let date1 = openDate ?? calendar.startOfDay(for:Date()),
+            date2 = expires ?? friday(after:date1),
+            timeInMarket = weekdaysBetween(date1,date2),
+            premium = strike * (pow(1.0 + (roi / 100), Double(timeInMarket)/PER_YEAR) - 1.0)
+        print("\(String(format: "%.2f",premium)) # Strike \(strike), roi \(roi), \(timeInMarket) days in market")
+    }
+}
+
+struct OptionReturns: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Utility calculations for options trading returns",
+        subcommands: [ShortPut.self, CoveredCall.self, TargetROI.self])
 }
 
 OptionReturns.main()
